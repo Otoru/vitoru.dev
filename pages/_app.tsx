bda1f47c9629bfe39db9bbb29f614da29fb98a6c
Vitor Hugo <vitor.hov@gmail.com>
2022-05-28 21:47:22 -0300

    :sparkles:  Add more PWA features to project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,31 @@
 import { appWithTranslation } from 'next-i18next'
 import { ChakraProvider } from 'lib/chakra'
 import type { AppProps } from 'next/app'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import theme from 'theme'
 
-const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <ChakraProvider theme={theme} cookies={pageProps.cookies}>
-    <Component {...pageProps} />
-  </ChakraProvider>
-)
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(
+          ({ scope }) => {
+            console.log(`🔔 Successful registration of SW with scope: ${scope}`)
+          },
+          (err) => {
+            console.error('⚠️ SW Registration failed:', err)
+          },
+        )
+      })
+    }
+  })
+
+  return (
+    <ChakraProvider theme={theme} cookies={pageProps.cookies}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
 
 export default appWithTranslation(App)
