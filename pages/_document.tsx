ac5f6bca138cdc193fadcd8bd7efd7fc7c2b1dd6
Vitor Hugo <vitor.hov@gmail.com>
2022-06-03 15:56:56 -0300

    :lipstick:  Changing font source

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
-import { Html, Head, Main, NextScript } from 'next/document'
 import { ColorModeScript, useColorModeValue } from '@chakra-ui/react'
+import { Html, Head, Main, NextScript } from 'next/document'
 import theme from 'theme'
 
 const Document = () => {
@@ -7,10 +7,6 @@ const Document = () => {
   return (
     <Html lang="en">
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Maven+Pro&family=Maven+Pro&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Lato:wght@500;600;700;900&display=swap"
-          rel="stylesheet"
-        />
         <link
           rel="apple-touch-icon"
           sizes="57x57"
